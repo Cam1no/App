41648121d46ec7f2bf655b9d3b78e88075b03dfb
kinjoyuji <kinjoyuji@gmail.com>
2017-04-08 12:26:11 +0900

    generatorの設定

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,16 @@ Bundler.require(*Rails.groups)
 
 module Denken3
   class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets true
+      g.javascripts false
+      g.helper false
+      g.template_engine :slim
+      g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
+      g.fixture_replacement :factory_girl, dir: "spec/support/factories"
+    end
+    config.sass.preferred_syntax = :sass
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
