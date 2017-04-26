0d767ffc2412239ee4601c50fe7d72cf92a4c2bc
kinjoyuji <kinjoyuji@gmail.com>
2017-04-27 01:31:03 +0900

    会員登録時に名前も登録できるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   helper_method :current_user
 
@@ -16,5 +17,9 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user!
     authenticate_basis!
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
